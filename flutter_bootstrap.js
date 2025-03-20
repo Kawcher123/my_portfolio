ff2cf2a5240940580abacece2d698c07234fa7c3
Kawsar Ahmed <kawcherahmed43@gmail.com>
2025-03-20 12:21:45 +0600

    ison added

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1317936422"
+    serviceWorkerVersion: "1727057356"
   }
 });
